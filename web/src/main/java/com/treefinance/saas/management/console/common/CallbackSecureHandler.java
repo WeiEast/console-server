e000b3b2c8b8ee30fcaad409d0dff037f14748f0
yh-treefinance <yehui@treefinance.com.cn>
2017-07-18 20:09:54 +0800

    增加数据下载接口

--- web/src/main/java/com/treefinance/saas/management/console/common/CallbackSecureHandler.java
+++ web/src/main/java/com/treefinance/saas/management/console/common/CallbackSecureHandler.java
@@ -1,11 +1,13 @@
 package com.treefinance.saas.management.console.common;
 
+import com.alibaba.fastjson.JSON;
 import com.datatrees.toolkits.util.crypto.RSA;
 import com.datatrees.toolkits.util.crypto.core.Decryptor;
 import com.datatrees.toolkits.util.crypto.core.Encryptor;
 import com.datatrees.toolkits.util.json.Jackson;
 import com.treefinance.saas.management.console.web.auth.exception.CallbackEncryptException;
 import com.treefinance.saas.management.console.web.auth.exception.CryptorException;
+import com.treefinance.saas.monitor.common.utils.AESSecureUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,68 +20,124 @@ import org.springframework.stereotype.Service;
 public class CallbackSecureHandler {
     private static final Logger logger = LoggerFactory.getLogger(CallbackSecureHandler.class);
 
-    private Encryptor getEncryptor(String publicKey) {
-        try {
-            if (StringUtils.isEmpty(publicKey)) {
-                throw new IllegalArgumentException("Can not find commercial tenant's public key.");
-            }
-
-            return RSA.createEncryptor(publicKey);
-        } catch (Exception e) {
-            throw new CryptorException(
-                    "Error creating Encryptor with publicKey '" + publicKey + " to encrypt callback.", e);
-        }
-    }
-
+    /**
+     * RSA 加密
+     *
+     * @param data
+     * @param publicKey
+     * @return
+     * @throws CallbackEncryptException
+     */
     public String encrypt(Object data, String publicKey) throws CallbackEncryptException {
         if (data == null) {
             return null;
         }
-        String encryptedData = encryptResult(data, publicKey);
+        String encryptedData = CallbackSecureHandler.Helper.encryptResult(data, publicKey);
         logger.debug("Finish encrypting callback for encryptedData '{}'.", encryptedData);
 
         return encryptedData;
     }
 
-    private String encryptResult(Object data, String publicKey) throws CallbackEncryptException {
-        Encryptor encryptor = getEncryptor(publicKey);
-        try {
-            byte[] json = Jackson.toJSONByteArray(data);
-            return encryptor.encryptAsBase64String(json);
-        } catch (Exception e) {
-            throw new CallbackEncryptException("Error encrypting callback data", e);
-        }
-    }
-
-    private Decryptor getDecryptor(String privateKey) {
-        try {
-            if (StringUtils.isEmpty(privateKey)) {
-                throw new IllegalArgumentException("Can not find commercial tenant's private key.");
-            }
-            return RSA.createDecryptor(privateKey);
-        } catch (Exception e) {
-            throw new CryptorException(
-                    "Error creating Decryptor with privateKey '" + privateKey + " to encrypt callback.", e);
-        }
-    }
-
-    private String decryptResult(Object data, String privateKey) throws CallbackEncryptException {
-        Decryptor decryptor = getDecryptor(privateKey);
-        try {
-            byte[] json = Jackson.toJSONByteArray(data);
-            return decryptor.decryptWithBase64AsString(json);
-        } catch (Exception e) {
-            throw new CallbackEncryptException("Error decrypting callback data", e);
-        }
-    }
-
+    /**
+     * RSA 解密
+     *
+     * @param data
+     * @param privateKey
+     * @return
+     * @throws CallbackEncryptException
+     */
     public String decrypt(Object data, String privateKey) throws CallbackEncryptException {
         if (data == null) {
             return null;
         }
-        String decryptedData = decryptResult(data, privateKey);
+        String decryptedData = CallbackSecureHandler.Helper.decryptResult(data, privateKey);
         logger.debug("Finish decrypting callback for decryptedData '{}'.", decryptedData);
 
         return decryptedData;
+    }
+
+    /**
+     * AES 解密
+     *
+     * @param data
+     * @param dataKey
+     * @return
+     */
+    public String decryptByAES(byte[] data, String dataKey) throws CallbackEncryptException {
+        try {
+            return AESSecureUtils.decrypt(dataKey, data);
+        } catch (Exception e) {
+            throw new CallbackEncryptException("decryptByAES exception", e);
+        }
+    }
+
+    /**
+     * AES 加密
+     *
+     * @param data
+     * @param dataKey
+     * @return
+     */
+    public String encryptByAES(Object data, String dataKey) throws CallbackEncryptException {
+        try {
+            String dataStr = JSON.toJSONString(data);
+            byte[] encryData = AESSecureUtils.encrypt(dataKey, dataStr.getBytes());
+            return new String(encryData);
+        } catch (Exception e) {
+            throw new CallbackEncryptException("encryptByAES exception", e);
+        }
+    }
+
+
+    /**
+     * 辅助类
+     */
+    static class Helper {
+
+
+        public static Encryptor getEncryptor(String publicKey) {
+            try {
+                if (StringUtils.isEmpty(publicKey)) {
+                    throw new IllegalArgumentException("Can not find commercial tenant's public key.");
+                }
+
+                return RSA.createEncryptor(publicKey);
+            } catch (Exception e) {
+                throw new CryptorException(
+                        "Error creating Encryptor with publicKey '" + publicKey + " to encrypt callback.", e);
+            }
+        }
+
+        public static String encryptResult(Object data, String publicKey) throws CallbackEncryptException {
+            Encryptor encryptor = getEncryptor(publicKey);
+            try {
+                byte[] json = Jackson.toJSONByteArray(data);
+                return encryptor.encryptAsBase64String(json);
+            } catch (Exception e) {
+                throw new CallbackEncryptException("Error encrypting callback data", e);
+            }
+        }
+
+        public static Decryptor getDecryptor(String privateKey) {
+            try {
+                if (StringUtils.isEmpty(privateKey)) {
+                    throw new IllegalArgumentException("Can not find commercial tenant's private key.");
+                }
+                return RSA.createDecryptor(privateKey);
+            } catch (Exception e) {
+                throw new CryptorException(
+                        "Error creating Decryptor with privateKey '" + privateKey + " to encrypt callback.", e);
+            }
+        }
+
+        public static String decryptResult(Object data, String privateKey) throws CallbackEncryptException {
+            Decryptor decryptor = getDecryptor(privateKey);
+            try {
+                byte[] json = Jackson.toJSONByteArray(data);
+                return decryptor.decryptWithBase64AsString(json);
+            } catch (Exception e) {
+                throw new CallbackEncryptException("Error decrypting callback data", e);
+            }
+        }
     }
 }
---
+++ web/src/main/java/com/treefinance/saas/management/console/web/controller/DataController.java
@@ -0,0 +1,47 @@
+package com.treefinance.saas.management.console.web.controller;
+
+import com.treefinance.saas.management.console.common.CallbackSecureHandler;
+import com.treefinance.saas.management.console.common.result.CommonStateCode;
+import com.treefinance.saas.management.console.common.result.Result;
+import com.treefinance.saas.management.console.common.result.Results;
+import com.treefinance.saas.monitor.common.utils.RemoteDataDownloadUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yh-treefinance on 2017/7/18.
+ */
+@RestController
+@RequestMapping("/saas/console/data/")
+public class DataController {
+    private static final Logger logger = LoggerFactory.getLogger(DataController.class);
+
+    @Autowired
+    protected CallbackSecureHandler callbackSecureHandler;
+
+    /**
+     * @param dataUrl
+     * @param key
+     * @return
+     */
+    @RequestMapping(value = "download", produces = "application/json", method = RequestMethod.POST)
+    public Result<String> downloadData(@RequestParam("dataUrl") String dataUrl,
+                                       @RequestParam("key") String key) {
+        // oss 下载数据
+        byte[] result = new byte[0];
+        try {
+            result = RemoteDataDownloadUtils.download(dataUrl, byte[].class);
+            // 数据体默认使用商户密钥加密
+            String data = callbackSecureHandler.decryptByAES(result, key);
+            return Results.newSuccessResult(data);
+        } catch (Exception e) {
+            logger.error("downloadData failed", e);
+            return Results.newFailedResult(e.getMessage(), CommonStateCode.FAILURE);
+        }
+    }
+}

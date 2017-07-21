08a2564b92717ad10fc77a8ecf02000919ae53cf
haojiahong <haojiahong@treefinance.com.cn>
2017-07-21 11:11:36 +0800

    回调管理

---
+++ biz/src/main/java/com/treefinance/saas/management/console/biz/service/AppCallbackConfigService.java
@@ -0,0 +1,9 @@
+package com.treefinance.saas.management.console.biz.service;
+
+/**
+ * 商户回调配置
+ * Created by haojiahong on 2017/7/21.
+ */
+public interface AppCallbackConfigService {
+
+}
---
+++ web/src/main/java/com/treefinance/saas/management/console/web/controller/AppCallbackConfigController.java
@@ -0,0 +1,16 @@
+package com.treefinance.saas.management.console.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by haojiahong on 2017/7/21.
+ */
+@RestController
+@RequestMapping("/saas/console/callback")
+public class AppCallbackConfigController {
+
+
+
+
+}

408087df4842872678cda8628a657c57565d9d3c
haojiahong <haojiahong@treefinance.com.cn>
2017-08-03 16:34:04 +0800

    console访问统计 成功量和成功率

---
+++ common/src/main/java/com/treefinance/saas/management/console/common/domain/vo/ChartStatRateVO.java
@@ -0,0 +1,33 @@
+package com.treefinance.saas.management.console.common.domain.vo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by haojiahong on 2017/7/7.
+ */
+public class ChartStatRateVO implements Serializable {
+
+    private static final long serialVersionUID = -4734492990487958188L;
+
+    private Date dataTime;//时间 x轴
+
+    private BigDecimal dataValue;//统计量 y轴
+
+    public Date getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(Date dataTime) {
+        this.dataTime = dataTime;
+    }
+
+    public BigDecimal getDataValue() {
+        return dataValue;
+    }
+
+    public void setDataValue(BigDecimal dataValue) {
+        this.dataValue = dataValue;
+    }
+}

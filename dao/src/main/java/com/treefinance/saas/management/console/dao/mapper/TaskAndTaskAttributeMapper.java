bb40f0e1d90b02e15f6bea5e4e2f7397eec5267f
haojiahong <haojiahong@treefinance.com.cn>
2017-12-27 15:24:16 +0800

    任务总览详情

---
+++ dao/src/main/java/com/treefinance/saas/management/console/dao/mapper/TaskAndTaskAttributeMapper.java
@@ -0,0 +1,17 @@
+package com.treefinance.saas.management.console.dao.mapper;
+
+import com.treefinance.saas.management.console.dao.entity.TaskAndTaskAttribute;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by haojiahong on 2017/12/27.
+ */
+public interface TaskAndTaskAttributeMapper {
+
+    long countByExample(Map<String, Object> map);
+
+    List<TaskAndTaskAttribute> getByExample(Map<String, Object> map);
+
+}

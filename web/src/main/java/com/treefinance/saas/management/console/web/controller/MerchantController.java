8b2d456633b09658ececae9bee4eb287a9941b59
guoguoyun <guoguoyun@treefinance.com.cn>
2018-01-18 10:44:55 +0800

    加入请求参数不能为空 抛出异常

--- web/src/main/java/com/treefinance/saas/management/console/web/controller/MerchantController.java
+++ web/src/main/java/com/treefinance/saas/management/console/web/controller/MerchantController.java
@@ -8,6 +8,7 @@ import com.treefinance.saas.management.console.common.domain.vo.MerchantSimpleVO
 import com.treefinance.saas.management.console.common.result.PageRequest;
 import com.treefinance.saas.management.console.common.result.Result;
 import com.treefinance.saas.management.console.common.result.Results;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -97,7 +98,9 @@ public class MerchantController {
 
     @RequestMapping(value = "stat/merchant/list", method = RequestMethod.GET)
     public Object queryAllEcommerceMonitor(Integer bizType) {
-
+        if (bizType == null ) {
+            throw new IllegalArgumentException("请求参数不能为空！");
+        }
         logger.info("电商列表查询 Controller层  传入参数为{}", bizType);
         return ecommerceMonitorService.queryAllEcommerceListByBizType(bizType);
 

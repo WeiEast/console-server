05606e7b42e65c65e057808abada518696d1ccb2
guoguoyun <guoguoyun@treefinance.com.cn>
2018-01-22 20:44:55 +0800

    请求参数为空判断 抛出异常

--- web/src/main/java/com/treefinance/saas/management/console/web/controller/EcmmerceStatController.java
+++ web/src/main/java/com/treefinance/saas/management/console/web/controller/EcmmerceStatController.java
@@ -2,6 +2,7 @@ package com.treefinance.saas.management.console.web.controller;
 
 import com.treefinance.saas.management.console.biz.service.EcommerceMonitorService;
 import com.treefinance.saas.management.console.common.domain.request.OperatorStatRequest;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,9 @@ public class EcmmerceStatController {
     @RequestMapping(value = "all/detail/list", method = RequestMethod.GET, produces = "application/json")
     public Object queryDivisionEcommerceMonitor(OperatorStatRequest request) {
 
+        if (request.getDataDate() == null || request.getStatType() == null ||request.getSourceType() == null || StringUtils.isBlank(request.getAppId())) {
+            throw new IllegalArgumentException("请求参数不能为空！");
+        }
         logger.info("电商分时详细查询 Controller层  传入参数为{}", request.toString());
         return ecommerceMonitorService.queryDivisionEcommerceMonitorList(request);
 
@@ -36,6 +40,9 @@ public class EcmmerceStatController {
     @RequestMapping(value ="all/day/list", method = RequestMethod.GET, produces = "application/json")
     public Object queryAllEcommerceMonitor(OperatorStatRequest request) {
 
+        if (request.getStartDate() == null ||request.getEndDate() == null ||request.getSourceType() == null || request.getStatType() == null || StringUtils.isBlank(request.getAppId())) {
+            throw new IllegalArgumentException("请求参数不能为空！");
+        }
         logger.info("电商整体查询 Controller层  传入参数为{}", request.toString());
         return ecommerceMonitorService.queryAllEcommerceMonitorList(request);
 

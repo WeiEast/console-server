b49ca75716d23a49e3000ee1f82e7b9277c7fbc2
hongyi@treefinance.com.con <hongyi1992>
2018-01-24 15:59:00 +0800

    gson格式化json

--- biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/DsDataApiRawResultSerivceImpl.java
+++ biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/DsDataApiRawResultSerivceImpl.java
@@ -3,7 +3,7 @@ package com.treefinance.saas.management.console.biz.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.treefinance.saas.dataservice.dataserver.dataapirawresult.dto.DataApiRawResultDTO;
 import com.treefinance.saas.dataservice.dataserver.dataapirawresult.facade.DataApiRawResultFacade;
 import com.treefinance.saas.dataservice.dataserver.dataapirawresult.request.DataApiRawResultRequest;
@@ -42,64 +42,67 @@ public class DsDataApiRawResultSerivceImpl implements DsDataApiRawResultSerivce 
     @Override
     public Result<Map<String, Object>> findPageByExample(DsDataApiRequest request) {
         DataApiRawResultRequest dataApiRawResultRequest = new DataApiRawResultRequest();
-        BeanUtils.copyProperties(request,dataApiRawResultRequest);
+        BeanUtils.copyProperties(request, dataApiRawResultRequest);
         List<DataApiRawResultDTO> dtoList = dataApiRawResultFacade.query(dataApiRawResultRequest);
         List<DataApiRawResultVO> voList = dtoList.stream().map(dto -> convert2VO(dto)).collect(Collectors.toList());
         return Results.newSuccessPageResult(request, dataApiRawResultFacade.count(dataApiRawResultRequest), voList);
     }
 
 
-
-    private DataApiRawResultVO convert2VO(DataApiRawResultDTO dto){
-        DataApiRawResultVO vo =  new DataApiRawResultVO();
-        BeanUtils.copyProperties(dto,vo);
+    private DataApiRawResultVO convert2VO(DataApiRawResultDTO dto) {
+        DataApiRawResultVO vo = new DataApiRawResultVO();
+        BeanUtils.copyProperties(dto, vo);
         String ossUrl = vo.getParamsStorePath();
-        String ossjson = getOssJson(dto.getAppId(),ossUrl);
-        if(StringUtils.isNotBlank(ossjson)) {
-           JSONObject jsonObject = JSON.parseObject(ossjson);
-            vo.setRequesetJson(jsonMerge((JSONObject)jsonObject.get("requestParams")));
-            vo.setResponseJson(jsonMerge((JSONObject)jsonObject.get("responseParams")));
+        String ossjson = getOssJson(dto.getAppId(), ossUrl);
+        if (StringUtils.isNotBlank(ossjson)) {
+            JSONObject jsonObject = JSON.parseObject(ossjson);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonParser jsonParser = new JsonParser();
+
+            JsonElement requestJsonElement = jsonParser.parse(jsonObject.getString("requestParams"));
+            vo.setRequesetJson(gson.toJson(requestJsonElement));
+
+            JsonElement responseJsonElement = jsonParser.parse(jsonObject.getString("responseParams"));
+            vo.setResponseJson(gson.toJson(responseJsonElement));
+
             vo.setErrorMsg(jsonObject.getString("detailErrorMsg"));
         }
         return vo;
     }
 
-    private String getOssJson(String appId,String ossUrl){
+    private String getOssJson(String appId, String ossUrl) {
         AppLicenseDTO appLicenseDTO = appLicenseService.selectOneByAppId(appId);
         String secretKey = appLicenseDTO.getDataSecretKey();
         try {
             byte[] result = RemoteDataDownloadUtils.download(ossUrl, byte[].class);
             if (result == null) {
-                logger.info("oss下载数据失败,数据为空,ossUrl - {}",ossUrl);
+                logger.info("oss下载数据失败,数据为空,ossUrl - {}", ossUrl);
                 return null;
             }
             String data = callbackSecureHandler.decryptByAES(result, secretKey);
             return data;
         } catch (Exception e) {
-            logger.error("oss下载解密数据失败,ossUrl - {}",ossUrl, e);
+            logger.error("oss下载解密数据失败,ossUrl - {}", ossUrl, e);
             return null;
         }
     }
 
 
-    private String jsonMerge(JSONObject jsonObject){
-        Map<String,Object> map =  jsonObject;
-        map.forEach((k,v) ->{
-            if(v instanceof Long){
-                map.put(k,String.valueOf(v));
+    private String jsonMerge(JSONObject jsonObject) {
+        Map<String, Object> map = jsonObject;
+        map.forEach((k, v) -> {
+            if (v instanceof Long) {
+                map.put(k, String.valueOf(v));
             }
-            if(v instanceof JSONObject){
+            if (v instanceof JSONObject) {
                 jsonMerge((JSONObject) v);
             }
-            if (v instanceof JSONArray){
-                ((JSONArray)v).forEach(j -> jsonMerge((JSONObject) j));
+            if (v instanceof JSONArray) {
+                ((JSONArray) v).forEach(j -> jsonMerge((JSONObject) j));
             }
         });
         return JSON.toJSONString(map);
     }
-//    public static void main(String[] args) {
-//        String json = "{\"data\":{\"blackPhones\":135433042558070784},\"taskId\":135433042558070784,\"timestamp\":[{\"blackPhones\":135433042558070784},{\"blackPhones\":135433042558070784}]}";
-//        System.out.println(jsonMerge(JSON.parseObject(json)));
-//    }
 
 }

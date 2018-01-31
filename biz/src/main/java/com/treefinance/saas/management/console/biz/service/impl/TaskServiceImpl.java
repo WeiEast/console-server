e27d5e2eacd0198bac2ff61129a3ed4651b9711b
haojiahong <haojiahong@treefinance.com.cn>
2018-01-31 16:56:45 +0800

    前端回调,dataUrl数据下载

--- biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/TaskServiceImpl.java
+++ biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/TaskServiceImpl.java
@@ -199,24 +199,36 @@ public class TaskServiceImpl implements TaskService {
                 logger.error("解密回调参数时,在任务回调记录表中未找到对应的任务信息 taskId={}", log.getTaskId());
                 continue;
             }
-            AppCallbackConfig appCallbackConfig = appCallbackConfigMap.get(log.getConfigId().intValue());
-            if (appCallbackConfig == null) {
-                logger.error("解密回调参数时,任务回调记录表中未找到对应的商户回调配置信息 configId={}", log.getConfigId());
-                continue;
+            //后端回调
+            if (log.getType() == 1) {
+                AppCallbackConfig appCallbackConfig = appCallbackConfigMap.get(log.getConfigId().intValue());
+                if (appCallbackConfig == null) {
+                    logger.error("解密回调参数时,任务回调记录表中未找到对应的商户回调配置信息 configId={}", log.getConfigId());
+                    continue;
+                }
+                if (ECallBackDataType.MAIN.getCode().equals(appCallbackConfig.getDataType())) {
+                    Long taskId = task.getId();
+                    TaskCallbackLogDTO logDTO = new TaskCallbackLogDTO();
+                    BeanUtils.convert(log, logDTO);
+                    logDTO.setPlainRequestParam(log.getRequestParam());
+                    //网关支持:一个任务,回调多方,这里现将日志打印出来
+                    if (result.get(taskId) != null) {
+                        logger.error("此taskId={},存在多个回调配置,TaskCallbackLogDTO={},otherTaskCallbackLogDTO={}", JSON.toJSONString(result.get(taskId)), logDTO);
+                        continue;
+                    }
+                    result.put(taskId, logDTO);
+                }
             }
-            if (ECallBackDataType.MAIN.getCode().equals(appCallbackConfig.getDataType())) {
+            //前端回调
+            if (log.getType() == 2) {
                 Long taskId = task.getId();
-//            String plainParams = getPlainParamsCallbackLog(task, appCallbackConfig, log);
                 TaskCallbackLogDTO logDTO = new TaskCallbackLogDTO();
                 BeanUtils.convert(log, logDTO);
                 logDTO.setPlainRequestParam(log.getRequestParam());
-                //网关支持:一个任务,回调多方,这里现将日志打印出来
-                if (result.get(taskId) != null) {
-                    logger.error("此taskId={},存在多个回调配置,TaskCallbackLogDTO={},otherTaskCallbackLogDTO={}", JSON.toJSONString(result.get(taskId)), logDTO);
-                    continue;
-                }
                 result.put(taskId, logDTO);
+
             }
+
 
         }
         logger.info("解密回调参数时,得到的任务对应主流程回调数据,Result={}", JSON.toJSONString(result));

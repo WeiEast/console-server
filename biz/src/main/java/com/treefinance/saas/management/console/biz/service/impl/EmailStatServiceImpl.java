e4fe7a30478cd1d5d1d6eab9fd7656af5f3c13fb
chengtong <chengtong@treefinance.com.cn>
2018-03-16 10:47:18 +0800

    邮箱监控console

---
+++ biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/EmailStatServiceImpl.java
@@ -0,0 +1,84 @@
+package com.treefinance.saas.management.console.biz.service.impl;
+
+import com.alibaba.dubbo.rpc.RpcResult;
+import com.google.common.collect.Lists;
+import com.treefinance.saas.management.console.biz.service.EmailStatService;
+import com.treefinance.saas.management.console.common.domain.request.EmailStatRequest;
+import com.treefinance.saas.management.console.common.domain.vo.AllOperatorStatDayAccessVO;
+import com.treefinance.saas.management.console.common.result.Results;
+import com.treefinance.saas.management.console.common.utils.BeanUtils;
+import com.treefinance.saas.management.console.common.utils.DateUtils;
+import com.treefinance.saas.monitor.facade.domain.request.EmailStatAccessRequest;
+import com.treefinance.saas.monitor.facade.domain.result.MonitorResult;
+import com.treefinance.saas.monitor.facade.domain.ro.stat.email.EmailStatAccessBaseRO;
+import com.treefinance.saas.monitor.facade.service.stat.EmailStatAccessFacade;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author chengtong
+ * @date 18/3/15 15:26
+ */
+@Service
+public class EmailStatServiceImpl implements EmailStatService {
+
+    @Autowired
+    EmailStatAccessFacade emailStatAccessFacade;
+
+    @Override
+    public Object queryEmailMonitorDayAccessList(EmailStatRequest request) {
+
+        EmailStatAccessRequest rpcRequest = new EmailStatAccessRequest();
+
+        rpcRequest.setStartTime(DateUtils.getTodayBeginDate(request.getStartDate()));
+        rpcRequest.setEndTime(DateUtils.getTodayEndDate(request.getEndDate()));
+
+        rpcRequest.setPageSize(request.getPageSize());
+        rpcRequest.setPageNumber(request.getPageNumber());
+
+        rpcRequest.setStatType(request.getStatType());
+        rpcRequest.setAppId(request.getAppId());
+        rpcRequest.setEmail(request.getEmail());
+        MonitorResult<List<EmailStatAccessBaseRO>> rpcResult = emailStatAccessFacade.queryEmailStatDayAccessList(rpcRequest);
+        if (CollectionUtils.isEmpty(rpcResult.getData())) {
+            return Results.newSuccessPageResult(request, 0, rpcResult.getData());
+        }
+        return Results.newSuccessPageResult(request, rpcResult.getTotalCount(), rpcResult.getData());
+    }
+
+    @Override
+    public Object queryEmailMonitorDayAccessListDetail(EmailStatRequest request) {
+
+
+        EmailStatAccessRequest rpcRequest = new EmailStatAccessRequest();
+
+        rpcRequest.setStartTime(DateUtils.getTodayBeginDate(request.getDataDate()));
+        rpcRequest.setEndTime(DateUtils.getTodayEndDate(request.getDataDate()));
+
+        rpcRequest.setPageSize(request.getPageSize());
+        rpcRequest.setPageNumber(request.getPageNumber());
+
+        rpcRequest.setStatType(request.getStatType());
+        rpcRequest.setAppId(request.getAppId());
+        rpcRequest.setEmail(request.getAppId());
+        MonitorResult<List<EmailStatAccessBaseRO>> rpcResult = emailStatAccessFacade.queryEmailStatDayAccessListDetail(rpcRequest);
+        if (CollectionUtils.isEmpty(rpcResult.getData())) {
+            return Results.newSuccessPageResult(request, 0, rpcResult.getData());
+        }
+        return Results.newSuccessPageResult(request, rpcResult.getTotalCount(), rpcResult.getData());
+
+    }
+
+    @Override
+    public Object queryEmailSupportList(EmailStatRequest request) {
+        MonitorResult<List<String>> rpcResult = emailStatAccessFacade
+                .queryEmailSupportList(new EmailStatAccessRequest());
+        if (CollectionUtils.isEmpty(rpcResult.getData())) {
+            return Results.newSuccessPageResult(request, 0, rpcResult.getData());
+        }
+        return Results.newSuccessPageResult(request, rpcResult.getTotalCount(), rpcResult.getData());
+    }
+}

7e62651427899a57b2ab73423293bdb0a5852ad3
haojiahong <haojiahong@treefinance.com.cn>
2018-06-22 14:34:36 +0800

    实时监控

---
+++ web/src/main/java/com/treefinance/saas/management/console/web/controller/RealTimeStatController.java
@@ -0,0 +1,19 @@
+package com.treefinance.saas.management.console.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Good Luck Bro , No Bug !
+ * 任务实时监控功能
+ *
+ * @author haojiahong
+ * @date 2018/6/21
+ */
+@RestController
+@RequestMapping("/saas/console/real/time/stat")
+public class RealTimeStatController {
+
+
+
+}

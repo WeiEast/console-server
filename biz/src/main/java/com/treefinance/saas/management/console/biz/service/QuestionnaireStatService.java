16b89dcafdff4c53a42ec5b5de127b04c150f536
guoguoyun <guoguoyun@treefinance.com.cn>
2018-08-21 15:10:30 +0800

    问卷统计接口

--- biz/src/main/java/com/treefinance/saas/management/console/biz/service/QuestionnaireStatService.java
+++ biz/src/main/java/com/treefinance/saas/management/console/biz/service/QuestionnaireStatService.java
@@ -1,8 +1,9 @@
 package com.treefinance.saas.management.console.biz.service;
 
-/**
+import com.treefinance.saas.merchant.center.facade.request.console.QueryAppQuestionnaireStatisticsRequest; /**
  * @author:guoguoyun
  * @date:Created in 2018/8/21下午2:04
  */
 public interface QuestionnaireStatService {
+    Object queryAppQuestionnaireStatistics(QueryAppQuestionnaireStatisticsRequest request);
 }

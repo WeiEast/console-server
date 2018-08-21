e5ce4b8826981cc782cbd34d92aaeae080b11f06
guoguoyun <guoguoyun@treefinance.com.cn>
2018-08-21 14:09:18 +0800

    问卷统计接口

--- biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/QuestionnaireStatServiceImpl.java
+++ biz/src/main/java/com/treefinance/saas/management/console/biz/service/impl/QuestionnaireStatServiceImpl.java
@@ -1,10 +1,12 @@
 package com.treefinance.saas.management.console.biz.service.impl;
 
 import com.treefinance.saas.management.console.biz.service.QuestionnaireStatService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author:guoguoyun
  * @date:Created in 2018/8/21下午2:04
  */
+@Service
 public class QuestionnaireStatServiceImpl implements QuestionnaireStatService{
 }
--- web/src/main/java/com/treefinance/saas/management/console/web/controller/QuestionnaireStatController.java
+++ web/src/main/java/com/treefinance/saas/management/console/web/controller/QuestionnaireStatController.java
@@ -2,11 +2,13 @@ package com.treefinance.saas.management.console.web.controller;
 
 import com.treefinance.saas.management.console.biz.service.QuestionnaireStatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author:guoguoyun
  * @date:Created in 2018/8/21下午2:02
  */
+@RestController
 public class QuestionnaireStatController {
 
     @Autowired
